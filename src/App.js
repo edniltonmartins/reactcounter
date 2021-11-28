956ea142cf09b7b751cafba0a9f7172aa993e87f
EdniltonMartins <ednilton.martins@equiplano.com.br>
2021-11-28 15:01:51 -0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handleMinusClick = () =>{
+    if (count>0){
+      setCount(count - 1);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Contador: {count}</h1>
+      <button 
+        className="minus" 
+        onClick={handleMinusClick}>-</button>
+      {"  "}
+      <button className="plus" onClick={()=>setCount(count+1)}>+</button>
+    </>
   );
 }
 
